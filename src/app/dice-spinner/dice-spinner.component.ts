af54c55f6b8cfcb4cbddb0139204e2347e594e7b
Peter Rogers <peter.rogers@gmail.com>
2020-10-04 23:00:03 -0400

    Refactor code into Spinner class

--- src/app/dice-spinner/dice-spinner.component.ts
+++ src/app/dice-spinner/dice-spinner.component.ts
@@ -22,6 +22,81 @@ function millis()
     return (new Date()).getTime()/1000.0;
 }
 
+class SpeedEstimator
+{
+}
+
+class Spinner
+{
+    startX : number;
+    lastDragPos : number = -1;
+    startTrayPos : number = 0;
+    // lastTime : number;
+    // velocity : number;
+
+    constructor(
+        private tray : HTMLElement
+    )
+    { }
+
+    setTrayX(value : number)
+    {
+        const rect = this.tray.getBoundingClientRect();
+        this.startTrayPos = value;
+
+        if (-this.startTrayPos > rect.width)
+        {
+            this.startTrayPos += rect.width;
+            this.tray.style.transform = 'translateX(' + this.startTrayPos + 'px)';
+
+            const tile = this.tray.querySelector('div');
+            this.tray.removeChild(tile);
+            this.tray.appendChild(tile);
+        }
+    }
+
+    startDrag(pos)
+    {
+        this.startX = pos;
+        // this.lastTime = millis();
+        this.lastDragPos = -1;
+        this.velocity = 0;
+    }
+
+    stopDrag(pos)
+    {
+        this.setTrayX(this.startTrayPos + pos - this.startX);
+        //
+        // let callback = () => {
+        //     const delay = 25;
+        //     velocity *= 0.98;
+        //
+        //     setTrayX(startTrayPos + velocity*(delay/1000.0));
+        //     tray.style.transform = 'translateX(' + startTrayPos + 'px)';
+        //
+        //     if (Math.abs(velocity) > 10) {
+        //         setTimeout(callback, delay);
+        //     }
+        // };
+        // callback();
+    }
+
+    drag(pos : number)
+    {
+        const now = millis();
+        const x = this.startTrayPos + pos - this.startX;
+        this.tray.style.transform = 'translateX(' + x + 'px)';
+
+        // if (this.lastDragPos !== -1) {
+        //     const w = 0.50;
+        //     velocity = w*velocity + (1-w)*(eventX - lastDragPos) / (now - lastTime);
+        // }
+
+        this.lastDragPos = pos;
+        // this.lastTime = now;
+    }
+}
+
 @Component({
     selector: 'app-dice-spinner',
     templateUrl: './dice-spinner.component.html',
@@ -35,11 +110,7 @@ export class DiceSpinnerComponent implements OnInit
     @ViewChild('tray', { static: true})
     trayView : any;
 
-    startX : number;
-    lastTime : number;
-    lastClientX : number = -1;
-    velocity : number;
-    startTrayX : number = 0;
+    spinner : Spinner;
 
     constructor() { }
 
@@ -48,66 +119,26 @@ export class DiceSpinnerComponent implements OnInit
         return this.trayView.nativeElement;
     }
 
-    ngOnInit(): void {
-    }
-
-    setTrayX(value : number)
+    ngOnInit(): void
     {
-        const rect = this.tray.getBoundingClientRect();
-        this.startTrayX = value;
-
-        if (-this.startTrayX > rect.width)
-        {
-            this.startTrayX += rect.width;
-            this.tray.style.transform = 'translateX(' + this.startTrayX + 'px)';
-
-            const tile = this.tray.querySelector('div');
-            this.tray.removeChild(tile);
-            this.tray.appendChild(tile);
-        }
+        this.spinner = new Spinner(this.tray);
     }
 
     handleMouseMove(event)
     {
         if (event.buttons === 1)
         {
-            const now = millis();
-            const x = this.startTrayX + event.clientX - this.startX;
-            this.tray.style.transform = 'translateX(' + x + 'px)';
-
-            // if (this.lastClientX !== null) {
-            //     const w = 0.50;
-            //     velocity = w*velocity + (1-w)*(eventX - lastClientX) / (now - lastTime);
-            // }
-
-            this.lastClientX = event.clientX;
-            this.lastTime = now;
+            this.spinner.drag(event.clientX);
         }
     }
 
     handleMouseUp(event)
     {
-        this.setTrayX(this.startTrayX + event.clientX - this.startX);
-        //
-        // let callback = () => {
-        //     const delay = 25;
-        //     velocity *= 0.98;
-        //
-        //     setTrayX(startTrayX + velocity*(delay/1000.0));
-        //     tray.style.transform = 'translateX(' + startTrayX + 'px)';
-        //
-        //     if (Math.abs(velocity) > 10) {
-        //         setTimeout(callback, delay);
-        //     }
-        // };
-        // callback();
+        this.spinner.stopDrag(event.clientX);
     }
 
     handleMouseDown(event)
     {
-        this.startX = event.clientX;
-        this.lastTime = millis();
-        this.lastClientX = null;
-        this.velocity = 0;
+        this.spinner.startDrag(event.clientX);
     }
 }
